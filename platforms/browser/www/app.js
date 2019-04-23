e0bfa23301e9650f640727436bbee53fa7d392ee
BaoShyang <sjsyphonetography@gmail.com>
2019-04-23 17:14:15 +0800

    'update'

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -1,3 +1,28 @@
-function sayHello() {
-  ons.notification.alert('你好吗');
+function showAlert() {
+  ons.notification.alert('This is a alert');
 }
+
+// function sayHello() {
+//   showToast();
+// }
+
+// var showToast = function(){
+//   ons.notification.toast('此讯息会自动消除', { timeout: 2000 });
+// }
+
+function showToast() {
+  ons.notification.toast('Toast!', { timeout: 2000 });
+}
+
+function showConfirm() {
+  ons.notification.confirm('请确认').then(function(input) {
+    ons.notification.alert('You clicked:' + input);
+  });
+}
+
+var showPrompt = function() {
+  ons.notification.prompt('Prompt!').then(function(input) {
+    var message = input ? 'Entered: ' + input : 'Entered nothing!';
+    ons.notification.alert(message);
+  });
+};
